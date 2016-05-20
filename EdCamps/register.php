898aeb18e2371591dba0dd08c5b6ab9b00f91971
Aleisha <nelson.aleisha@gmail.com>
2016-05-19 19:56:59 -0700

    Update register.php

--- EdCamps/register.php
+++ EdCamps/register.php
@@ -290,10 +290,10 @@
 		<?php
 ini_set('display_errors','On');
 error_reporting(E_ALL);
-$db_host = "dbserver.engr.scu.edu";
-$db_user = "gchrist";
-$db_pass = "00001011346";
-$db_name = "sdb_gchrist";
+$db_host = "dbserver.********";
+$db_user = "******";
+$db_pass = "***********";
+$db_name = "*********";
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 // Check connection
